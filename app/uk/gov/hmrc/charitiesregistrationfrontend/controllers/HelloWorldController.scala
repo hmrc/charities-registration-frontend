9e03c9b16c5a8f0b4cff841a969bd37780f0101a
Jenkins user <jenkins@ip-10-35-73-118.eu-west-2.compute.internal>
2020-03-17 10:55:18 +0000

    Creating new service charities-registration-frontend

---
+++ app/uk/gov/hmrc/charitiesregistrationfrontend/controllers/HelloWorldController.scala
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.charitiesregistrationfrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import uk.gov.hmrc.charitiesregistrationfrontend.config.AppConfig
+import uk.gov.hmrc.charitiesregistrationfrontend.views.html.hello_world
+
+import scala.concurrent.Future
+
+@Singleton
+class HelloWorldController @Inject()(appConfig: AppConfig, mcc: MessagesControllerComponents)
+    extends FrontendController(mcc) {
+
+  implicit val config: AppConfig = appConfig
+
+  val helloWorld: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(hello_world()))
+  }
+
+}

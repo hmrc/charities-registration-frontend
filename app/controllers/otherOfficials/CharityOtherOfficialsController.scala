fba54ee284a789e14f4e35c5594ac0be18bb799c
“abhijeetardale” <26648207+abhijeetardale@users.noreply.github.com>
2020-07-22 16:05:04 +0000

    DDCE-816: changes for P1 - Charity’s other officials

---
+++ app/controllers/otherOfficials/CharityOtherOfficialsController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.otherOfficials
+
+import config.FrontendAppConfig
+import controllers.LocalBaseController
+import controllers.actions._
+import javax.inject.Inject
+import models.Index
+import play.api.mvc._
+import repositories.UserAnswerRepository
+import views.html.otherOfficials.CharityOtherOfficialsView
+
+import scala.concurrent.Future
+
+
+class CharityOtherOfficialsController @Inject()(
+    val userAnswerRepository: UserAnswerRepository,
+    identify: AuthIdentifierAction,
+    getData: UserDataRetrievalAction,
+    requireData: DataRequiredAction,
+    val controllerComponents: MessagesControllerComponents,
+    view: CharityOtherOfficialsView
+   )(implicit appConfig: FrontendAppConfig) extends LocalBaseController {
+
+  def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+
+    Future.successful(Ok(view(Index(0))))
+  }
+}
---
+++ test/views/otherOfficials/CharityOtherOfficialsViewSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.otherOfficials
+
+import assets.messages.BaseMessages
+import models.Index
+import play.twirl.api.HtmlFormat
+import views.behaviours.ViewBehaviours
+import views.html.otherOfficials.CharityOtherOfficialsView
+
+class CharityOtherOfficialsViewSpec extends ViewBehaviours  {
+
+  private val messageKeyPrefix = "charityOtherOfficials"
+
+    "CharityOtherOfficialsView" must {
+
+      def applyView(): HtmlFormat.Appendable = {
+        val view = viewFor[CharityOtherOfficialsView](Some(emptyUserAnswers))
+        view.apply(Index(0))(fakeRequest, messages, frontendAppConfig)
+      }
+
+      behave like normalPage(applyView(), messageKeyPrefix, section = Some(messages("officialsAndNominees.section")))
+
+      behave like pageWithAdditionalGuidance(applyView(), messageKeyPrefix,
+        "p1", "p2", "p3", "p4")
+
+      behave like pageWithBackLink(applyView())
+
+      behave like pageWithHyperLink(applyView(), "linkButton",controllers.routes.DeadEndController.onPageLoad().url,BaseMessages.continue)
+
+
+    }
+  }

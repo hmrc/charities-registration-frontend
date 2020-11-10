b8a552852b17ce6c0ba56ad488ca94dc370e9b03
“abhijeetardale” <26648207+abhijeetardale@users.noreply.github.com>
2020-11-10 10:44:46 +0000

    DDCE-1299: changes to implement Updated Task List Design

--- app/utils/TaskListHelper.scala
+++ app/utils/TaskListHelper.scala
@@ -22,68 +22,87 @@ import controllers.nominees.{routes => charityNomineeRoutes}
 import controllers.operationsAndFunds.{routes => opsAndFundsRoutes}
 import controllers.otherOfficials.{routes => otherOfficialsRoutes}
 import controllers.regulatorsAndDocuments.{routes => regulatorDocsRoutes}
-import models.{NormalMode, UserAnswers}
+import models.UserAnswers
 import pages.QuestionPage
 import pages.sections._
 import play.api.mvc.Call
 import viewmodels.TaskListRow
 
+
 class TaskListHelper {
 
-  def getTaskListRow(implicit userAnswers: UserAnswers): List[TaskListRow] = {
-
-    val section1 = getSection("index.section1.spoke1.label",
+  implicit class TaskList(val userAnswers: UserAnswers) {
+    implicit val ua = userAnswers
+    val section1: () => TaskListRow = () => getSection("index.section1.spoke1.label",
       charityInfoRoutes.StartInformationController.onPageLoad(),
       charityInfoRoutes.CharityInformationSummaryController.onPageLoad(),
       Section1Page)
 
-    val section2 = getSection("index.section2.spoke1.label",
+    val section2: () => TaskListRow = () => getSection("index.section2.spoke1.label",
       regulatorDocsRoutes.StartCharityRegulatorController.onPageLoad(),
       regulatorDocsRoutes.RegulatorsSummaryController.onPageLoad(),
-      Section2Page)
+      Section2Page)(userAnswers)
 
-    val section3 = getSection("index.section2.spoke2.label",
+    val section3: () => TaskListRow = () => getSection("index.section2.spoke2.label",
       regulatorDocsRoutes.StartGoverningDocumentController.onPageLoad(),
       regulatorDocsRoutes.GoverningDocumentSummaryController.onPageLoad(),
-      Section3Page)
+      Section3Page)(userAnswers)
 
-    val section4 = getSection("index.section3.spoke1.label",
+    val section4: () => TaskListRow = () => getSection("index.section3.spoke1.label",
       opsAndFundsRoutes.StartCharitableObjectivesController.onPageLoad(),
       opsAndFundsRoutes.CharityObjectivesSummaryController.onPageLoad(),
-      Section4Page)
+      Section4Page)(userAnswers)
 
-    val section5 = getSection("index.section3.spoke2.label",
+    val section5: () => TaskListRow = () => getSection("index.section3.spoke2.label",
       opsAndFundsRoutes.StartFundraisingController.onPageLoad(),
       opsAndFundsRoutes.OperationsFundsSummaryController.onPageLoad(),
-      Section5Page)
+      Section5Page)(userAnswers)
 
-    val section6 = getSection("index.section3.spoke3.label",
+    val section6: Boolean => TaskListRow = (isDependent: Boolean) => getSection("index.section3.spoke3.label",
       opsAndFundsRoutes.StartBankDetailsController.onPageLoad(),
       opsAndFundsRoutes.BankDetailsSummaryController.onPageLoad(),
-      Section6Page)
+      Section6Page,
+      Some(isDependent)
+    )(userAnswers)
 
-    val section7 = getSection("index.section4.spoke1.label",
+    val section7: () => TaskListRow = () => getSection("index.section4.spoke1.label",
       authOfficialsRoutes.CharityAuthorisedOfficialsController.onPageLoad(),
       authOfficialsRoutes.AuthorisedOfficialsSummaryController.onPageLoad(),
-      Section7Page)
+      Section7Page)(userAnswers)
 
-    val section8 = getSection("index.section4.spoke2.label",
+    val section8: () => TaskListRow = () => getSection("index.section4.spoke2.label",
       otherOfficialsRoutes.CharityOtherOfficialsController.onPageLoad(),
       otherOfficialsRoutes.OtherOfficialsSummaryController.onPageLoad(),
-      Section8Page)
+      Section8Page)(userAnswers)
 
-    val section9 = getSection("index.section4.spoke3.label",
+    val section9: () => TaskListRow = () => getSection("index.section4.spoke3.label",
       charityNomineeRoutes.CharityNomineeController.onPageLoad(),
       charityNomineeRoutes.NomineeDetailsSummaryController.onPageLoad(),
-      Section9Page)
+      Section9Page)(userAnswers)
 
-    List(section1, section2, section3, section4, section5, section6, section7, section8, section9)
-
+    val section10: Boolean => TaskListRow = (isDependent: Boolean) => {
+      TaskListRow(
+        "index.section5.spoke1.label",
+        controllers.routes.DeclarationController.onPageLoad(),
+        if (isDependent) "index.section.notStarted" else "index.section.canNotStartYet")
+    }
   }
 
-  private def getSection(name: String, normalUrl: Call, changeUrl: Call, sectionId: QuestionPage[Boolean])(
-    implicit userAnswers: UserAnswers) : TaskListRow = {
+  def getTaskListRow(implicit userAnswers: UserAnswers): List[TaskListRow] = {
+
+    val result = List(userAnswers.section1(), userAnswers.section2(), userAnswers.section3(), userAnswers.section4(),
+      userAnswers.section5(), userAnswers.section6(userAnswers.section1().state.contains("completed")),
+      userAnswers.userAnswers.section7(), userAnswers.section8(), userAnswers.section9())
+
+    val declaration = userAnswers.section10(result.forall(_.state.equals("index.section.completed")))
+
+    result ++ List(declaration)
+  }
+
+  private def getSection(name: String, normalUrl: Call, changeUrl: Call, sectionId: QuestionPage[Boolean],
+                         isDependent: Option[Boolean] = None)(implicit userAnswers: UserAnswers) : TaskListRow = {
     userAnswers.get(sectionId) match {
+      case _ if isDependent.contains(false) => TaskListRow(name, normalUrl, "index.section.canNotStartYet")
       case Some(true) => TaskListRow(name, changeUrl, "index.section.completed")
       case Some(false) => TaskListRow(name, normalUrl, "index.section.inProgress")
       case _ => TaskListRow(name, normalUrl, "index.section.notStarted")
--- test/utils/TaskListHelperSpec.scala
+++ test/utils/TaskListHelperSpec.scala
@@ -18,72 +18,141 @@ package utils
 
 import base.SpecBase
 import models.UserAnswers
-import pages.sections.{Section1Page, Section2Page}
+import pages.sections._
 import viewmodels.TaskListRow
 
 class TaskListHelperSpec extends SpecBase {
 
   private val helper = new TaskListHelper
 
-  private val section1 = TaskListRow("index.section1.spoke1.label",
-    controllers.charityInformation.routes.StartInformationController.onPageLoad(),
-    "index.section.notStarted")
+  private def section1(status:String = "notStarted") = TaskListRow("index.section1.spoke1.label",
+    if(status=="completed") {
+      controllers.charityInformation.routes.CharityInformationSummaryController.onPageLoad()
+    } else {
+      controllers.charityInformation.routes.StartInformationController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section1Competed = TaskListRow("index.section1.spoke1.label",
-    controllers.charityInformation.routes.CharityInformationSummaryController.onPageLoad(),
-    "index.section.completed")
+  private def section2(status:String = "notStarted") = TaskListRow("index.section2.spoke1.label",
+    if(status=="completed") {
+      controllers.regulatorsAndDocuments.routes.RegulatorsSummaryController.onPageLoad()
+    } else {
+      controllers.regulatorsAndDocuments.routes.StartCharityRegulatorController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section2 = TaskListRow("index.section2.spoke1.label",
-    controllers.regulatorsAndDocuments.routes.StartCharityRegulatorController.onPageLoad(),
-    "index.section.notStarted")
+  private def section3(status:String = "notStarted") = TaskListRow("index.section2.spoke2.label",
+    if(status=="completed") {
+      controllers.regulatorsAndDocuments.routes.GoverningDocumentSummaryController.onPageLoad()
+    } else {
+      controllers.regulatorsAndDocuments.routes.StartGoverningDocumentController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section2InProgress = TaskListRow("index.section2.spoke1.label",
-    controllers.regulatorsAndDocuments.routes.StartCharityRegulatorController.onPageLoad(),
-    "index.section.inProgress")
+  private def section4(status:String = "notStarted") = TaskListRow("index.section3.spoke1.label",
+    if(status=="completed") {
+      controllers.operationsAndFunds.routes.CharityObjectivesSummaryController.onPageLoad()
+    } else {
+      controllers.operationsAndFunds.routes.StartCharitableObjectivesController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section3 = TaskListRow("index.section2.spoke2.label",
-    controllers.regulatorsAndDocuments.routes.StartGoverningDocumentController.onPageLoad(),
-    "index.section.notStarted")
+  private def section5(status:String = "notStarted") = TaskListRow("index.section3.spoke2.label",
+    if(status=="completed") {
+      controllers.operationsAndFunds.routes.OperationsFundsSummaryController.onPageLoad()
+    } else {
+      controllers.operationsAndFunds.routes.StartFundraisingController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section4 = TaskListRow("index.section3.spoke1.label",
-    controllers.operationsAndFunds.routes.StartCharitableObjectivesController.onPageLoad(),
-    "index.section.notStarted")
+  private def section6(status:String = "canNotStartYet") = TaskListRow("index.section3.spoke3.label",
+    if(status=="completed") {
+      controllers.operationsAndFunds.routes.BankDetailsSummaryController.onPageLoad()
+    } else {
+      controllers.operationsAndFunds.routes.StartBankDetailsController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section5 = TaskListRow("index.section3.spoke2.label",
-    controllers.operationsAndFunds.routes.StartFundraisingController.onPageLoad(),
-    "index.section.notStarted")
+  private def section7(status:String = "notStarted") = TaskListRow("index.section4.spoke1.label",
+    if(status=="completed") {
+      controllers.authorisedOfficials.routes.AuthorisedOfficialsSummaryController.onPageLoad()
+    } else {
+      controllers.authorisedOfficials.routes.CharityAuthorisedOfficialsController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section6 = TaskListRow("index.section3.spoke3.label",
-    controllers.operationsAndFunds.routes.StartBankDetailsController.onPageLoad(),
-    "index.section.notStarted")
+  private def section8(status:String = "notStarted") = TaskListRow("index.section4.spoke2.label",
+    if(status=="completed") {
+      controllers.otherOfficials.routes.OtherOfficialsSummaryController.onPageLoad()
+    } else {
+      controllers.otherOfficials.routes.CharityOtherOfficialsController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section7 = TaskListRow("index.section4.spoke1.label",
-    controllers.authorisedOfficials.routes.CharityAuthorisedOfficialsController.onPageLoad(),
-    "index.section.notStarted")
+  private def section9(status:String = "notStarted") = TaskListRow("index.section4.spoke3.label",
+    if(status=="completed") {
+      controllers.nominees.routes.NomineeDetailsSummaryController.onPageLoad()
+    } else {
+      controllers.nominees.routes.CharityNomineeController.onPageLoad()
+    },
+    s"index.section.$status")
 
-  private val section8 = TaskListRow("index.section4.spoke2.label",
-    controllers.otherOfficials.routes.CharityOtherOfficialsController.onPageLoad(),
-    "index.section.notStarted")
-
-  private val section9 = TaskListRow("index.section4.spoke3.label",
-    controllers.nominees.routes.CharityNomineeController.onPageLoad(),
-    "index.section.notStarted")
+  private def section10(status:String = "canNotStartYet") = TaskListRow("index.section5.spoke1.label",
+    controllers.routes.DeclarationController.onPageLoad(),
+    s"index.section.$status")
 
   "getTaskListRow" must {
 
-    "return all rows with not started status" in {
+    "return all rows with not started and Cannot start yet status" in {
 
       implicit val userAnswers: UserAnswers = emptyUserAnswers
 
-      helper.getTaskListRow mustBe List(section1, section2, section3, section4, section5, section6, section7, section8, section9)
+      helper.getTaskListRow mustBe List(section1(), section2(), section3(), section4(), section5(),
+        section6(), section7(), section8(), section9(), section10())
+    }
+
+    "return all rows with completed(section1), not started and Cannot start yet(section10) status" in {
+
+      implicit val userAnswers: UserAnswers = emptyUserAnswers.set(Section1Page, true).success.value
+
+      helper.getTaskListRow mustBe List(section1("completed"), section2(), section3(), section4(), section5(),
+        section6("notStarted"), section7(), section8(), section9(), section10())
+    }
+
+    "return all rows with completed(section1), inProgress(Section6), not started and Cannot start yet(section10) status" in {
+
+      implicit val userAnswers: UserAnswers = emptyUserAnswers.set(Section1Page, true).flatMap(
+        _.set(Section6Page, false)).success.value
+
+      helper.getTaskListRow mustBe List(section1("completed"), section2(), section3(), section4(), section5(),
+        section6("inProgress"), section7(), section8(), section9(), section10())
     }
 
     "return all rows with mix status depending upon the status flag" in {
 
       implicit val userAnswers: UserAnswers = emptyUserAnswers.set(Section1Page, true).flatMap(
-        _.set(Section2Page, false)).success.value
+        _.set(Section2Page, false)).flatMap(
+        _.set(Section6Page, false)).success.value
 
-      helper.getTaskListRow mustBe List(section1Competed, section2InProgress, section3, section4, section5, section6, section7, section8, section9)
+      helper.getTaskListRow mustBe List(section1("completed"), section2("inProgress"), section3(), section4(),
+        section5(), section6("inProgress"), section7(), section8(), section9(), section10())
+    }
+
+    "return all rows with completed status except Section 10 with not started status" in {
+
+      implicit val userAnswers: UserAnswers = emptyUserAnswers.set(Section1Page, true).flatMap(
+        _.set(Section2Page, true)).flatMap(
+        _.set(Section3Page, true)).flatMap(
+        _.set(Section4Page, true)).flatMap(
+        _.set(Section5Page, true)).flatMap(
+        _.set(Section6Page, true)).flatMap(
+        _.set(Section7Page, true)).flatMap(
+        _.set(Section8Page, true)).flatMap(
+        _.set(Section9Page, true)).success.value
+
+      helper.getTaskListRow mustBe List(section1("completed"), section2("completed"), section3("completed"),
+        section4("completed"), section5("completed"), section6("completed"), section7("completed"),
+        section8("completed"), section9("completed"), section10("notStarted"))
     }
 
   }

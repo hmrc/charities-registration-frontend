3aa95f457b14d8afd54f6b886cefc9ca8d72396e
mywyau <47248104+mywyau@users.noreply.github.com>
2023-02-20 13:55:08 +0000

    DDCE-3559 Bump to latest scala version 2.13.10

--- app/models/transformers/TransformerKeeper.scala
+++ app/models/transformers/TransformerKeeper.scala
@@ -17,5 +17,6 @@
 package models.transformers
 
 import play.api.libs.json.{JsObject, JsPath, JsonValidationError}
+import scala.collection.Seq
 
 case class TransformerKeeper(accumulator: JsObject, errors: Seq[(JsPath, Seq[JsonValidationError])])

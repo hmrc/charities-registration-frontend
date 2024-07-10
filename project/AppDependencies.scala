f2b7ed4e89491e62a7a29eae78cd8884540a4db6
Taiwo-AB <63714619+Taiwo-AB@users.noreply.github.com>
2024-07-10 15:14:17 +0100

    DDCE-5458 Fix warning text issues (#493)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,20 +2,20 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val mongoHmrcVersion     = "2.0.0"
+  private lazy val mongoHmrcVersion     = "2.1.0"
   private lazy val bootstrapPlayVersion = "8.6.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoHmrcVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "10.1.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "10.3.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoHmrcVersion,
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
-    "org.jsoup"          % "jsoup"                   % "1.17.2",
-    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.18.0"
+    "org.jsoup"          % "jsoup"                   % "1.18.1",
+    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID]    = Seq.empty

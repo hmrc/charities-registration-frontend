7326db978fbdb17f88b86baa535e049bf62fa2bc
lmugnai <11977337+lmugnai@users.noreply.github.com>
2024-12-16 15:09:29 +0000

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,13 @@ object AppDependencies {
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoHmrcVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.5.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.7.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoHmrcVersion,
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
-    "org.jsoup"          % "jsoup"                   % "1.18.1",
+    "org.jsoup"          % "jsoup"                   % "1.18.3",
     "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0"
   ).map(_ % Test)
 

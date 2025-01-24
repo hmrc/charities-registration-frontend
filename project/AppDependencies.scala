78c869d357790770cfbd7c9c09509a972904bf75
lmugnai <11977337+lmugnai@users.noreply.github.com>
2025-01-24 16:50:35 +0000

    Update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val mongoHmrcVersion     = "2.3.0"
-  private lazy val bootstrapPlayVersion = "9.5.0"
+  private lazy val mongoHmrcVersion     = "2.4.0"
+  private lazy val bootstrapPlayVersion = "9.7.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoHmrcVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.7.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.11.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(

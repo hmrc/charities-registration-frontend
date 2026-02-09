3d8d0a1339f20fb7a62ef4debfb9a2e1234ae41c
PabloJSartirana <243117200+PabloJSartirana@users.noreply.github.com>
2026-02-09 10:51:04 +0000

    DLSN-371: Updated librares versions: - uk.gov.hmrc:play-frontend-hmrc-play-30 - uk.gov.hmrc:play-frontend-hmrc-test-30 - uk.gov.hmrc.mongo:hmrc-mongo-play-30

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val mongoHmrcVersion     = "2.11.0"
+  private lazy val mongoHmrcVersion     = "2.12.0"
   private lazy val bootstrapPlayVersion = "10.5.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoHmrcVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.27.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.29.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
